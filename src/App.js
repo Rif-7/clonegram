0a480e180f74e54a99d88e0de6be63e4f85bb6eb
Rif-7 <muhammadrifan974530@gmail.com>
2022-04-24 10:45:01 +0530

    initialize repo with neccessary dependencies and configurations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSieWCEMtVfdXjD8j0LvFllv5G3KgrApo",
+  authDomain: "clom-c8a86.firebaseapp.com",
+  projectId: "clom-c8a86",
+  storageBucket: "clom-c8a86.appspot.com",
+  messagingSenderId: "275927124881",
+  appId: "1:275927124881:web:e65a7e1eeacd2a13936830",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
